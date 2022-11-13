440feb9e73af9fe6b77e3be0b382156aa5d2df66
Panchenko Vitaliy <90642321+FIOWB21@users.noreply.github.com>
2022-11-14 00:53:35 +0200

    Add files via upload

---
+++ ConsoleApplication33.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+
+    const int size = 88;
+    static int a[size];
+    int* arr = new int [size] {};
+    cout << "---- Bubble sort ---- \n" << "Before: " << "\n\n";
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand() % 100;
+    }
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\n\n" << "After: " << "\n\n";
+
+    int temp;
+
+    for (int i = size / 2; i < size; i++) {
+        for (int j = size / 2; j < size - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+/////////////////////////////////////////////////////////////////////////
+
+    cout << "\n\n" << "---- Selection sort ---- \n" << "Before: " << "\n\n";
+    int* arr2 = arr;
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr2[i] << " ";
+    }
+    cout << "\n\n" << "After: \n\n";
+    for (int i = size / 2; i < size; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr2[j] > arr2[min])
+                min = j;
+        }
+        int tmp = arr2[min];
+        arr2[min] = arr2[i];
+        arr2[i] = tmp;
+    }
+    for (int i = 0; i < size; ++i)
+        cout << arr2[i] << ' ';
+    cout << endl;
+    
+
+    return 0;
+
+}
---
+++ ConsoleApplication34.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+
+void Shell_sort(int* a, int n)
+{
+    cout << endl << "After: \n";
+    int step = n / 2;
+    while (step > 0)
+    {
+        for (int i = 0; i < n - step; i++)
+        {
+            int j = i;
+            while (j >= 0 && a[j] > a[j + step]) 
+            {
+                int temp = a[j];
+                a[j] = a[j + step];
+                a[j + step] = temp;
+                j--;
+            }
+        }
+        step = step / 2;
+    }
+}
+
+void insertSort(int* a, int n)
+{
+   
+    cout << "---- Insert sort ---- \n" << "Before: \n";
+    {
+        for (int i = 0; i < n; i++)
+            cout << a[i] << " ";
+    }
+
+    cout << "\n\nAfter: \n";
+    int tmp;
+    for (int i = 1, j; i < n; ++i) 
+    {
+        tmp = a[i];
+        for (j = i - 1; j >= 0 && a[j] > tmp; --j) 
+            a[j + 1] = a[j];    
+        a[j + 1] = tmp;    
+    }
+    for (int i = 0; i < n; i++)
+          cout << a[i] << " ";
+
+}
+
+int main()
+{
+    srand(time(NULL));
+
+    const int size = 72;
+    int* arr = new int[size];
+    int k = 0;
+    cout << "---- Shell sort ---- \n" << "Before: \n";
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand() % 100;
+    }
+    
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    Shell_sort(arr, size);
+
+    for (int i = 0; i < size; i++)
+        if (arr[i] != 0) arr[k++] = arr[i];
+
+    for (int i = k; i < size; i++)
+        arr[i] = 0;
+
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << "\n\n";
+    
+    
+    insertSort(arr, size);
+
+    cout << endl;
+
+    
+    return 0;
+}
